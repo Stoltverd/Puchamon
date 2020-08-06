9324915ab6a46f9d8826071831797a396cd3943a
Metz001 <46971952+Metz001@users.noreply.github.com>
2020-08-06 08:06:44 -0500

    Skill y Critter creados
    Skill vacío y Critter con atributos y constructor

---
+++ Puchamon/Critter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Puchamon
+{
+    class Critter
+    {
+        //Atributos
+        string name;
+        byte baseAtk, baseDef, daseDef, hp;
+        
+        enum Afinity
+        {
+            fire, earth, wind, dark, water, light
+        }
+        List<Skill> moveSet = new List<Skill>();
+        //Accesores
+        public String Name { get => name; set => name = value; }
+        public byte BaseAtk { get => baseAtk; set => baseAtk = value; }
+        public byte BaseDef { get => baseDef; set => baseDef = value; }
+        public byte DaseDef { get => daseDef; set => daseDef = value; }
+        public byte Hp { get => hp; set => hp = value; }
+        public List<Skill> MoveSet { get => moveSet; set => moveSet = value; }
+
+        public Critter(string name, byte baseAtk, byte baseDef, byte daseDef, byte hp, List<Skill> moveSet)
+        {
+            this.Name = name;
+            this.BaseAtk = baseAtk;
+            this.BaseDef = baseDef;
+            this.DaseDef = daseDef;
+            this.Hp = hp;
+            this.MoveSet = moveSet;
+        }
+    }
+}
