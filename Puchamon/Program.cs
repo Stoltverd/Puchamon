54798a1c5d4f9d4ef594c2ddf7b25e02d97b8306
Esteban Martínez Villegas <esteban.m.v@gmail.com>
2020-08-06 23:50:03 -0500

    Cambio de colección
    Yo soy la negra

--- Puchamon/Program.cs
+++ Puchamon/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Puchamon
 {
@@ -17,7 +18,23 @@ namespace Puchamon
             /*Se crean 2 puchamones para cada player
             Critter biggus = new Critter("Biggus Dickus", 80, 10, 2, 200,)*/
 
-
+            //Chequea la hp del criter peleando y lo pasa de colección apenas se chorrea
+            foreach (var critter in player.Critters)
+            {
+                if (critter.Hp <= 0)
+                {
+                    opponent.Critters.Add(critter);
+                    player.Critters.Remove(critter);
+                }
+            }
+            foreach (var critter in opponent.Critters)
+            {
+                if (critter.Hp <= 0)
+                {
+                    player.Critters.Add(critter);
+                    opponent.Critters.Remove(critter);
+                }
+            }
 
         }
 
